1cd97b2042b63d1cc922b1a748f14d50f42e5806
max pleaner <maxpleaner@gmail.com>
2016-06-13 21:51:31 +0000

    $argv

--- client/main.js
+++ client/main.js
@@ -3,63 +3,74 @@ import { ReactiveVar } from 'meteor/reactive-var';
 
 import './main.html';
 
+// Subscribe to a list of online users
+// Unless a user is signed in, this publication will be an empty array
 Meteor.startup(function(){
     Meteor.subscribe("userStatus")
 })
 
-const MAP_ZOOM = 15;
-
+// Load the Google Map when the template is rendered.
 Template.map.onRendered(function() {
-  console.log("map rendered")
   GoogleMaps.load( { v: '3', key: 'AIzaSyD_eSUViM3ppuaIXVzpCnkVELAPYQ2XWMY' } );
 });
 
+// Initialization options for Google Maps
 Template.map.helpers({
   exampleMapOptions: function() {
     if (GoogleMaps.loaded()) {
-      return {
-        center: new google.maps.LatLng(-37.8136, 144.9631),
-        zoom: 1
-      };
+      return { center: new google.maps.LatLng(-37.8136, 144.9631), zoom: 1 };
     }
   },
 });
 
 Template.map.onCreated(function(){
+  // Pass "this" to other scopes by assigning it a variable
   var self = this
+  // Callback when Google Maps is ready
   GoogleMaps.ready('map', function(map) {
+    // An autorun scope will re-evaluate if reactive variables inside it change.
+    // Here, there are two reactive variables:
+    //     - Geolocation.latlng()
+    //     - the custom ReactiveVars in window.lat and window.lng
     self.autorun(function(){
-      // destroy all markers
+      // First, get all the users and put them on the map
+      // This lookup will only include online users, because only they are published
       Meteor.users.find(
         {}, { fields: { latitude: 1, longitude: 1, emails: 1 } }
       ).forEach(function(user){
-        if (!(self.markers)) { self.markers = {} }
+        // define a "markers" variable on the template which tracks which tracks user/marker association
+        if (!(self.markers)) { self.markers = {} } // initialize markers if undefined
         if (!(self.markers[user._id])) {
+          // if there isn't a marker for the user yet, create one
           self.markers[user._id] = new google.maps.Marker({
             position: new google.maps.LatLng(user.latitude, user.longitude),
             map: map.instance
           });
         } else {
+          // If there's already a marker for the user, update the position
           self.markers[user._id].setPosition({lat: user.latitude, lng: user.longitude})
         }
       })
+      // Next, setup reactive variables
       var latLng = Geolocation.latLng();
       if (! latLng) { return; }
       var user = Meteor.user()
       if (user) {
         if (!window.lat || !window.lng) {
+          // reactive variables stored on the window object
           window.lat = new ReactiveVar(latLng.lat), window.lng = new ReactiveVar(latLng.lng)
         }
-
         // introduce some variation into the geolocations
         // this block can be commented/uncommented.
         if (!(window.geolocationRandomnessInterval)) {
           window.geolocationRandomnessInterval = window.setInterval(function() {
-            window.lat.set(Math.random() * 180)
-            window.lng.set(Math.random() * 180)
+            window.lat.set(Math.random() * 50)
+            window.lng.set(Math.random() * 50)
           }, 1000)
         }
-
+        // Call updateUser at the end of the autorun block.
+        // Since the autorun function will rerun if one of its reactive variables changes,
+        // this updateUser call will be made with different values each time.
         Meteor.call("updateUser", {
           _id: user._id, latitude: lat.get(), longitude: lng.get()
         })
--- server/main.js
+++ server/main.js
@@ -1,40 +1,44 @@
 import { Meteor } from 'meteor/meteor';
 
-Meteor.startup(() => {
-  // code to run on server at startup
-});
-
-
-Accounts.onCreateUser(function(options, user) {
-    user.latitude = options.latitude || null
-    user.longitude = options.longitude || null
-    return user
-})
-
+// Publish a list of all signed in users. 
+// Only publish this to authenticated users
 Meteor.publish("userStatus", function() {
+  // If there is a current user signed in
   if (this.userId) {
+    // Publish all online users to the client.
     return Meteor.users.find(
+      // Use a lookup attribute is provided by the mizzao:meteor-user-status package
+      // to find which users are online
       { "status.online": true },
-      { fields: { latitude: 1, longitude: 1, email: 1 } }
+      // Specify custom fields to include while publishing
+      { fields: { latitude: 1, longitude: 1, emails: 1 } }
     );
+    // If no user is signed in, return an empty array
   } else { return [] }
 });
 
+// Methods which are called from the client
 Meteor.methods({
   updateUser(user) {
-    console.dir(user)
+    // If the user being edited is also signed in
     if (user._id && (Meteor.userId() == user._id)) {
       var id = user._id
       delete user._id
+      // Check that all the keys are allowed to be updated 
+      // The user._id key is first deleted so that this check can pass.
       var keysAreValid = Object.keys(user).every((field) => {
+        // Only allow latitude and longitude to be updated
         return ["latitude", "longitude"].indexOf(field) != -1
       })
       if (keysAreValid) {
+        // Send an update to the server if the keys are acceptable
         Meteor.users.update(id, { $set: user, });
       }  else {
+        // Throw an error if the keys are invalid
         throw new Error("invalid update fields to user")
       }
     } else {
+      // Throw an error if the user being updated isn't signed in
       throw new Error("invalid userId to update user")
     }
   }
