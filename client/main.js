f7ada455db5c1ee7fb34dce7d48f9507e6d4b3db
max pleaner <maxpleaner@gmail.com>
2016-06-13 20:35:53 +0000

    Got user marker on map. Also refactored this to call a server-side updateUser method and show updates on the map via the users subscription list.

--- client/main.js
+++ client/main.js
@@ -3,20 +3,56 @@ import { ReactiveVar } from 'meteor/reactive-var';
 
 import './main.html';
 
-Template.hello.helpers({
-  
-  usersOnline() {
-    return Meteor.users.find(
-      {"status.online": true },
-    )
-  },
-      
-  emailString(emails) {
-    return emails.map(email => {return email.address}).join(", ")
-  },
-    
+Meteor.startup(function(){
+    Meteor.subscribe("userStatus")
 })
 
-Template.hello.onCreated(function helloOnCreated() {
-  Meteor.subscribe("userStatus")
+const MAP_ZOOM = 15;
+
+Template.map.onRendered(function() {
+  console.log("map rendered")
+  GoogleMaps.load( { v: '3', key: 'AIzaSyD_eSUViM3ppuaIXVzpCnkVELAPYQ2XWMY' } );
 });
+
+Template.map.helpers({
+  exampleMapOptions: function() {
+    if (GoogleMaps.loaded()) {
+      return {
+        center: new google.maps.LatLng(-37.8136, 144.9631),
+        zoom: 1
+      };
+    }
+  },
+});
+
+Template.map.onCreated(function(){
+  var self = this
+  GoogleMaps.ready('map', function(map) {
+    self.autorun(function(){
+      // destroy all markers
+      Meteor.users.find(
+        {}, { fields: { latitude: 1, longitude: 1, emails: 1 } }
+      ).forEach(function(user){
+        console.log(user)
+        if (!(self.markers)) { self.markers = {} }
+        if (!(self.markers[user._id])) {
+          self.markers[user._id] = new google.maps.Marker({
+            position: new google.maps.LatLng(user.latitude, user.longitude),
+            map: map.instance
+          });
+        } else {
+          self.markers[user._id].setPosition({lat: user.latitude, lng: user.longitude})
+        }
+      })
+      var latLng = Geolocation.latLng();
+      if (! latLng) { return; }
+      var user = Meteor.user()
+      if (user) {
+        Meteor.call("updateUser", {
+//           _id: user._id, latitude: latLng.lat, longitude: latLng.lng
+          _id: user._id, latitude: (Math.random() * 180), longitude: (Math.random() * 180)
+        })
+      }
+    })
+  });
+})
--- server/main.js
+++ server/main.js
@@ -4,8 +4,38 @@ Meteor.startup(() => {
   // code to run on server at startup
 });
 
+
+Accounts.onCreateUser(function(options, user) {
+    user.latitude = options.latitude || null
+    user.longitude = options.longitude || null
+    return user
+})
+
 Meteor.publish("userStatus", function() {
-  return Meteor.users.find(
-    { "status.online": true },
-  );
+  if (this.userId) {
+    return Meteor.users.find(
+      { "status.online": true },
+      { fields: { latitude: 1, longitude: 1, email: 1 } }
+    );
+  } else { return [] }
 });
+
+Meteor.methods({
+  updateUser(user) {
+    console.dir(user)
+    if (user._id && (Meteor.userId() == user._id)) {
+      var id = user._id
+      delete user._id
+      var keysAreValid = Object.keys(user).every((field) => {
+        return ["latitude", "longitude"].indexOf(field) != -1
+      })
+      if (keysAreValid) {
+        Meteor.users.update(id, { $set: user, });
+      }  else {
+        throw new Error("invalid update fields to user")
+      }
+    } else {
+      throw new Error("invalid userId to update user")
+    }
+  }
+})
